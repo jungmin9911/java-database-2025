d093de6e1f2a9e352036ab53f716c63b779326d1
JungMin <jmahn1819@gmail.com>
2025-03-17 23:23:15 +0900

    저녁 공부
    배고픔
    프로그래머스 SQL 문제 계속 풀어보기

--- day03/sql01_복수행함수_마무리.sql
+++ day03/sql01_복수행함수_마무리.sql
@@ -3,13 +3,22 @@
 -- GROUP BY
 -- 그룹핑시 GROUP BY에 들어가는 컬럼만 SELECT 절에 사용가능
 -- WHERE 절은 일반적 조건만 비교, HAVING 집계함수를 조건에 사용할 때
+SELECT * FROM EMPLOYEES ORDER BY department_id;
+SELECT department_id, sum(salary) FROM employees GROUP BY department_id ORDER BY department_id;
+
+-- 부서ID가 70보다 크거나 같고, 급여합계가 20000보다 크거나 같은 사람의 부서ID와 급여합계를 급여합계의 오름차순으로 구하라 
 SELECT department_id
 	 , sum(salary)
   FROM employees
  WHERE department_id <= 70
-GROUP BY department_id
+ GROUP BY department_id
 HAVING sum(salary) <= 20000
 ORDER BY 2 ASC;  -- sum(salary)와2가 동일. (SELECT절 순서)
+
+/* GROUP BY와 함께 쓰이는 집계함수
+ * MAX(), MIN(), MEDIAN() - 최대값, 최소값, 중앙값
+ * SUM(), AVG() - 합, 평균
+ * VARIANCE(), STDDEV(), COUNT() - 분산, 표준편차, 관측값(row)수 */
 
 -- RANK()  -- 1, 2, 3, 4, 5 순서
 -- DENSE_RANK()  -- 1, 2, 2, 3 순서
